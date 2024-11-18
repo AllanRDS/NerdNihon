6570a71e69380675b821cde6e3aa913b9b277053
AllanRDS <allanreyemi@outlook.com>
2024-11-18 12:57:07 -0300

    Versão Beta Carrosel (Ainda a Fazer alterações)

--- src/app/Componentes/carrosel/carrosel.component.ts
+++ src/app/Componentes/carrosel/carrosel.component.ts
@@ -1,32 +1,46 @@
-import { Component } from '@angular/core';
-import { AnimeService } from '../../Services/anime.service';
+import { Component, OnInit } from '@angular/core';
+import { AnimeService } from '../../Services/anime.service'; // Serviço para obter dados dos animes
 
 @Component({
   selector: 'app-carrosel',
   templateUrl: './carrosel.component.html',
-  styleUrl: './carrosel.component.css'
+  styleUrls: ['./carrosel.component.css']
 })
-export class CarroselComponent {
-  constructor(private animeService : AnimeService){}
+export class CarroselComponent implements OnInit {
+  currentSlide: number = 0; // Slide atual
+  currentSeasonList: any[] = []; // Lista de animes da temporada atual
+  limitedAnimeList: any[] = []; // Lista limitada de animes a serem exibidos (max 5)
+  loading: boolean = true;
 
-  currentSeasonList: any[] = []
+  constructor(private animeService: AnimeService) {}
 
-  ngOnInit()
-  {
-    this.getCurrentSeason()
+  ngOnInit() {
+    this.getCurrentSeason(); // Carrega os animes da temporada atual
   }
 
-  getCurrentSeason()
-  {
+  getCurrentSeason() {
     this.animeService.getCurrentAnimeSeason().subscribe({
-        next: (response: any) => { // Use uma função anônima aqui
-            this.currentSeasonList = response.data;
-            console.log(this.currentSeasonList);
-        },
-        error: (error: any) => {
-            console.error('Erro ao carregar a temporada atual:', error);
-            alert("Erro ao carregar a lista da temporada atual");
-        }
+      next: (response: any) => {
+        this.currentSeasonList = response.data;
+        // Limitar a quantidade de animes a serem exibidos (exibindo no máximo 5)
+        this.limitedAnimeList = this.currentSeasonList.slice(0, 5);
+        this.loading = false;
+      },
+      error: (error: any) => {
+        console.error('Erro ao carregar a temporada atual:', error);
+        alert("Erro ao carregar a lista da temporada atual");
+        this.loading = false;
+      }
     });
+  }
+
+  // Função para avançar o slide
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.limitedAnimeList.length;
+  }
+
+  // Função para voltar o slide
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.limitedAnimeList.length) % this.limitedAnimeList.length;
   }
 }
