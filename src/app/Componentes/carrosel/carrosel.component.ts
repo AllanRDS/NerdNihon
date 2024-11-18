3c72fe4858656e9e0291d0ca647bd789baa74355
AllanRDS <allanreyemi@outlook.com>
2024-11-18 17:38:50 -0300

    Carrosel Versão 1.0

--- src/app/Componentes/carrosel/carrosel.component.ts
+++ src/app/Componentes/carrosel/carrosel.component.ts
@@ -1,46 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { AnimeService } from '../../Services/anime.service'; // Serviço para obter dados dos animes
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AnimeService } from '../../Services/anime.service';
 
 @Component({
   selector: 'app-carrosel',
   templateUrl: './carrosel.component.html',
   styleUrls: ['./carrosel.component.css']
 })
-export class CarroselComponent implements OnInit {
+export class CarroselComponent implements OnInit, OnDestroy {
   currentSlide: number = 0; // Slide atual
-  currentSeasonList: any[] = []; // Lista de animes da temporada atual
-  limitedAnimeList: any[] = []; // Lista limitada de animes a serem exibidos (max 5)
-  loading: boolean = true;
+  limitedAnimeList: any[] = []; // Lista limitada de animes a serem exibidos
+  slideInterval: any; // Intervalo para mudança automática de slides
 
   constructor(private animeService: AnimeService) {}
 
   ngOnInit() {
     this.getCurrentSeason(); // Carrega os animes da temporada atual
+    this.startSlideShow(); // Inicia a apresentação de slides automática
+  }
+
+  ngOnDestroy() {
+    this.stopSlideShow(); // Para a apresentação de slides automática ao destruir o componente
   }
 
   getCurrentSeason() {
     this.animeService.getCurrentAnimeSeason().subscribe({
       next: (response: any) => {
-        this.currentSeasonList = response.data;
-        // Limitar a quantidade de animes a serem exibidos (exibindo no máximo 5)
-        this.limitedAnimeList = this.currentSeasonList.slice(0, 5);
-        this.loading = false;
+        if (response && response.data) {
+          this.limitedAnimeList = response.data.slice(0, 5); // Limitar a quantidade de animes a serem exibidos (exibindo no máximo 5)
+          this.currentSlide = 0; // Reseta o slide atual após carregar os dados
+        } else {
+          console.error('Resposta inválida:', response);
+        }
       },
       error: (error: any) => {
         console.error('Erro ao carregar a temporada atual:', error);
         alert("Erro ao carregar a lista da temporada atual");
-        this.loading = false;
       }
     });
   }
 
+  // Inicia a apresentação de slides automática
+  startSlideShow() {
+    this.slideInterval = setInterval(() => {
+      this.nextSlide();
+    }, 10000); // Muda o slide a cada 20 segundos
+  }
+
+  // Para a apresentação de slides automática
+  stopSlideShow() {
+    if (this.slideInterval) {
+      clearInterval(this.slideInterval);
+    }
+  }
+
   // Função para avançar o slide
   nextSlide() {
-    this.currentSlide = (this.currentSlide + 1) % this.limitedAnimeList.length;
+    if (this.limitedAnimeList.length > 0) {
+      this.currentSlide = (this.currentSlide + 1) % this.limitedAnimeList.length;
+    }
   }
 
   // Função para voltar o slide
   prevSlide() {
-    this.currentSlide = (this.currentSlide - 1 + this.limitedAnimeList.length) % this.limitedAnimeList.length;
+    if (this.limitedAnimeList.length > 0) {
+      this.currentSlide = (this.currentSlide - 1 + this.limitedAnimeList.length) % this.limitedAnimeList.length;
+    }
   }
 }
