2b228c04f1e06ba2b3eea4b6d1c1f54f6efeaff3
AllanRDS <allanreyemi@outlook.com>
2024-11-20 16:49:21 -0300

    Preliminar animes-filmes

--- src/app/Paginas/anime/anime.component.ts
+++ src/app/Paginas/anime/anime.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AnimefilterService } from '../../Services/animefilter.service';
 
 @Component({
   selector: 'app-anime',
   templateUrl: './anime.component.html',
-  styleUrl: './anime.component.css'
+  styleUrls: ['./anime.component.css']
 })
-export class AnimeComponent {
+export class AnimeComponent implements OnInit {
+  animes: any[] = [];
+  searchTerm: string = '';
+  selectedGenre: string = '';
+  selectedYear: string = '';
+  isLoading: boolean = false;
+  currentPage: number = 1;
 
+  genres: any[] = [];
+  years: string[] = [];
+
+  constructor(private animeFilterService: AnimefilterService) {}
+
+  ngOnInit() {
+    this.loadGenres();
+    this.loadYears();
+    this.searchAnimes();
+  }
+
+  loadGenres() {
+    this.animeFilterService.getAnimeGenres().subscribe({
+      next: (response) => {
+        this.genres = response.data.map((genre: any) => genre.name);
+      },
+      error: (error) => {
+        console.error('Erro ao carregar gêneros', error);
+      }
+    });
+  }
+
+  loadYears() {
+    this.years = this.animeFilterService.getAnimeYears();
+  }
+
+  searchAnimes() {
+    this.currentPage = 1; // Adicionando esta linha
+    this.isLoading = true;
+    this.animeFilterService.getFilteredAnimes(
+      this.searchTerm,
+      this.currentPage,
+      this.selectedGenre,
+      this.selectedYear
+    ).subscribe({
+      next: (response) => {
+        this.animes = response.data;
+        this.isLoading = false;
+      },
+      error: (error) => {
+        console.error('Erro ao buscar animes', error);
+        this.isLoading = false;
+      }
+    });
+  }
+
+  applyFilter() {
+    this.currentPage = 1;
+    this.searchAnimes();
+  }
+
+  nextPage() {
+    this.currentPage++;
+    this.searchAnimes();
+  }
+
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.searchAnimes();
+    }
+  }
 }
--- src/app/Services/animefilter.service.ts
+++ src/app/Services/animefilter.service.ts
@@ -1,9 +1,59 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimefilterService {
+  apiLink = "https://api.jikan.moe/v4";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  // Método de filtro com parâmetros opcionais
+  getFilteredAnimes(
+    searchTerm: string = '',
+    page: number = 1,
+    genre: string = '',
+    year: string = ''
+  ): Observable<any> {
+    let url = `${this.apiLink}/anime?`;
+
+    // Parâmetros base
+    const params = new URLSearchParams();
+    params.append('sfw', 'true');
+    params.append('page', page.toString());
+
+    // Adiciona termo de busca se existir
+    if (searchTerm) {
+      params.append('q', searchTerm);
+    }
+
+    // Adiciona gênero se existir
+    if (genre) {
+      params.append('genres', genre);
+    }
+
+    // Adiciona ano se existir
+    if (year) {
+      params.append('start_date', `${year}-01-01`);
+      params.append('end_date', `${year}-12-31`);
+    }
+
+    return this.http.get<any>(`${url}${params.toString()}`);
+  }
+
+  // Lista de gêneros para o select
+  getAnimeGenres(): Observable<any> {
+    return this.http.get<any>(`${this.apiLink}/genres/anime`);
+  }
+
+  // Anos para o select (pode ser gerado dinamicamente)
+  getAnimeYears(): string[] {
+    const currentYear = new Date().getFullYear();
+    return Array.from(
+      {length: 10},
+      (_, i) => (currentYear - i).toString()
+    );
+  }
 }
